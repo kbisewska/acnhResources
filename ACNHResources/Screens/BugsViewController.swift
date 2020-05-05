73bfec5772596e4be6ea981168496908e74ff9be
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-05 12:13:17 +0200

    Add view controllers

---
+++ ACNHResources/Screens/BugsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BugsViewController.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 05/05/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class BugsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ACNHResources/Screens/VillagersViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VillagersViewController.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 05/05/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class VillagersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
