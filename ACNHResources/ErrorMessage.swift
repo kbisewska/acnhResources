20a1dd3cb9e507696b258a8aa2b4406cc2679223
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-06 14:01:38 +0200

    Create ErrorMessage enum

---
+++ ACNHResources/ErrorMessage.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorMessage.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 05/05/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorMessage: String, Error {
+    
+    case clientError = "Unable to complete your request. Please check your internet connection."
+    case serverError = "Invalid response from the server. Please try again."
+    case noDataReceived = "No data received. Please try again."
+    case invalidData = "The data received from the server is invalid. Please try again."
+}
