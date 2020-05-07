c487ea2c93a1a908f5826164b44e49d988e903ef
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-07 14:22:45 +0200

    Create methods in NetworkManager for getting images

--- ACNHResources/Managers/NetworkManager.swift
+++ ACNHResources/Managers/NetworkManager.swift
@@ -13,6 +13,8 @@ class NetworkManager {
     let baseURL = "http://acnhapi.com/"
     let urlSession = URLSession(configuration: .default)
     
+    // MARK: - Getting Data
+    
     func getVillagerData(completion: @escaping (Result<[String: Villager], ErrorMessage>) -> Void) {
         let urlRequest = URLRequest(url: URL(string: baseURL + "villagers")!)
         getResourceData(urlRequest: urlRequest, completion: completion)
@@ -35,32 +37,80 @@ class NetworkManager {
     
     func getResourceData<T: Codable>(urlRequest: URLRequest, completion: @escaping (Result<T, ErrorMessage>) -> Void) {
         let task = urlSession.dataTask(with: urlRequest) { data, response, error in
-            guard error == nil else {
-                completion(.failure(.clientError))
-                return                       
+            var resource: T?
+            var errorMessage: ErrorMessage?
+            
+            defer {
+                DispatchQueue.main.async {
+                    if let resource = resource {
+                        completion(.success(resource))
+                    } else if let errorMessage = errorMessage {
+                        completion(.failure(errorMessage))
+                    }
+                }
+            }
+            
+            guard error == nil, let data = data else {
+                errorMessage = .clientError
+                return
             }
             
             guard let response = response as? HTTPURLResponse,
                 200..<300 ~= response.statusCode else {
-                    completion(.failure(.serverError))
+                    errorMessage = .serverError
                     return
             }
             
-            guard let data = data else {
-                completion(.failure(.noDataReceived))
-                return
+            do {
+                resource = try JSONDecoder().decode(T.self, from: data)
+            } catch {
+                errorMessage = .invalidData
+            }
+        }
+        
+        task.resume()
+    }
+    
+    // MARK: - Getting Images
+    
+    func getVillagerImage(with id: Int, completion: @escaping (UIImage?) -> Void) {
+        let urlRequest = URLRequest(url: URL(string: baseURL + "images/villagers/\(id)")!)
+        getResourceImage(urlRequest: urlRequest, completion: completion)
+    }
+    
+    func getFishImage(with id: Int, completion: @escaping (UIImage?) -> Void) {
+        let urlRequest = URLRequest(url: URL(string: baseURL + "images/fish/\(id)")!)
+        getResourceImage(urlRequest: urlRequest, completion: completion)
+    }
+    
+    func getBugImage(with id: Int, completion: @escaping (UIImage?) -> Void) {
+        let urlRequest = URLRequest(url: URL(string: baseURL + "images/bugs/\(id)")!)
+        getResourceImage(urlRequest: urlRequest, completion: completion)
+    }
+    
+    func getFossilImage(with fileName: String, completion: @escaping (UIImage?) -> Void) {
+        let urlRequest = URLRequest(url: URL(string: baseURL + "images/fossils/\(fileName)")!)
+        getResourceImage(urlRequest: urlRequest, completion: completion)
+    }
+    
+    func getResourceImage(urlRequest: URLRequest, completion: @escaping (UIImage?) -> Void) {
+        let task = urlSession.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil,
+                let response = response as? HTTPURLResponse,
+                200..<300 ~= response.statusCode,
+                let data = data,
+                let image = UIImage(data: data) else {
+                    DispatchQueue.main.async {
+                       completion(nil)
+                    }
+                    return
             }
             
-            do {
-                let resource = try JSONDecoder().decode(T.self, from: data)
-                completion(.success(resource))
-            } catch {
-                completion(.failure(.invalidData))
+            DispatchQueue.main.async {
+                completion(image)
             }
         }
         
         task.resume()
     }
 }
-
-
