9ea0c0f1e72264d4c9f48e07c2968c9a1b5dd1d7
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-07 15:18:29 +0200

    Add ResourceCell

---
+++ ACNHResources/Cells/ResourceCell.swift
@@ -0,0 +1,13 @@
+//
+//  ResourceCell.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 07/05/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class ResourceCell: UITableViewCell {
+    
+}
