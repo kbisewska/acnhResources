1df36687ee350113f777c31ec79a8f3636a0691c
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-11 15:13:39 +0200

    Add downloadIcon extension to UIImageView

--- ACNHResources/Extensions/UIImageView+Ext.swift
+++ ACNHResources/Extensions/UIImageView+Ext.swift
@@ -14,6 +14,15 @@ enum Resource {
     case fish(id: Int)
     case bug(id: Int)
     case fossil(fileName: String)
+    
+    fileprivate var name: String {
+        switch self {
+        case .villager: return "villagers"
+        case .fish: return "fish"
+        case .bug: return "bugs"
+        case .fossil: return "fossils"
+        }
+    }
 }
 
 extension UIImageView {
@@ -49,6 +58,19 @@ extension UIImageView {
             
                 self.image = image
             }
+        }
+    }
+    
+    func downloadIcon(for resource: Resource) {
+        let networkManager = NetworkManager()
+        
+        switch resource {
+        case .bug(let id), .villager(let id), .fish(let id):
+            networkManager.getIcon(for: resource.name, id: id) { [weak self] icon in
+                guard let self = self else { return }
+                self.image = icon
+            }
+        case .fossil: return
         }
     }
 }
