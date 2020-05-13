6e6f79f4831c418fd052d991c97aa75a52477423
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-13 15:50:57 +0200

    Add resource details view controllers

---
+++ ACNHResources/ViewControllers/FishDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FishDetailsViewController.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 13/05/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class FishDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
