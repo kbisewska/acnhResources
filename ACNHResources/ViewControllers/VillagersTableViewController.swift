a2691e0c3e60e6f980b0171edc0ba12728dc243d
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-15 16:20:15 +0200

    Add VillagersTableViewController

---
+++ ACNHResources/ViewControllers/VillagersTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  VillagersTableViewController.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 15/05/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class VillagersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
