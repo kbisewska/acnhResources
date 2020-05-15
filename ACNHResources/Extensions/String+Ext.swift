a71e83575a6a967289cc2ed39b27bb6a684b07d2
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-15 16:11:48 +0200

    Add convertMonths extension to String

--- ACNHResources/Extensions/String+Ext.swift
+++ ACNHResources/Extensions/String+Ext.swift
@@ -13,4 +13,30 @@ extension String {
     func capitalizeFirstLetter() -> String {
         prefix(1).capitalized + dropFirst()
     }
+    
+    func convertMonths() -> String {
+        let months = self.components(separatedBy: "-")
+        var monthsNames = [String]()
+        
+        for month in months {
+            switch month {
+            case "1": monthsNames.append("January")
+            case "2": monthsNames.append("February")
+            case "3": monthsNames.append("March")
+            case "4": monthsNames.append("April")
+            case "5": monthsNames.append("May")
+            case "6": monthsNames.append("June")
+            case "7": monthsNames.append("July")
+            case "8": monthsNames.append("August")
+            case "9": monthsNames.append("September")
+            case "10": monthsNames.append("October")
+            case "11": monthsNames.append("November")
+            case "12": monthsNames.append("December")
+            default: break
+            }
+        }
+        
+        let convertedMonths = monthsNames.joined(separator: " - ")
+        return convertedMonths
+    }
 }
