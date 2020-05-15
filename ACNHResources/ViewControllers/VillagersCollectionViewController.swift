52b66dc45e03c9abbbe38ae32d772c87c836ce32
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-15 16:21:59 +0200

    Add VillagersCollectionViewController

---
+++ ACNHResources/ViewControllers/VillagersCollectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VillagersCollectionViewController.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 15/05/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class VillagersCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        return cell
+    }
+}
