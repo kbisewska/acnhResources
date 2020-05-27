a6d862839037d9108f6f28f89690945bea31fb63
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-05-27 15:16:07 +0200

    Add sha256 extension to String

--- ACNHResources/Extensions/String+Ext.swift
+++ ACNHResources/Extensions/String+Ext.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 kbisewska. All rights reserved.
 //
 
+import CryptoKit
 import Foundation
 
 extension String {
@@ -38,5 +39,10 @@ extension String {
         
         let convertedMonths = monthsNames.joined(separator: " - ")
         return convertedMonths
+    }
+    
+    func sha256() -> String {
+        return SHA256.hash(data: Data(self.utf8))
+            .compactMap { String(format: "%02x", $0) }.joined()
     }
 }
