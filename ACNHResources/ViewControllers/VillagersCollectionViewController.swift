b5991f858177ae32921e840bbc143b47c885e2ef
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-08 15:26:00 +0200

    Configure VillagersCollectionViewController

--- ACNHResources/ViewControllers/VillagersCollectionViewController.swift
+++ ACNHResources/ViewControllers/VillagersCollectionViewController.swift
@@ -2,34 +2,122 @@
 //  VillagersCollectionViewController.swift
 //  ACNHResources
 //
-//  Created by Kornelia Bisewska on 15/05/2020.
+//  Created by Kornelia Bisewska on 25/05/2020.
 //  Copyright © 2020 kbisewska. All rights reserved.
 //
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class VillagersCollectionViewController: UICollectionViewController {
-
+    
+    private let headerReuseIdentifier = "Header"
+    private let persistenceManager = PersistenceManager()
+    
+    var villagers = [Villager]()
+    var filteredVillagers = [Villager]()
+    var isFiltering = false
+    
+    init(with villagers: [Villager]) {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+        self.villagers = villagers
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionalLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerReuseIdentifier)
+        collectionView.register(VillagerCell.self, forCellWithReuseIdentifier: VillagerCell.reuseIdentifier)
+        
+        update()
     }
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 0
+    
+    func update() {
+        let ownedVillagers: [Villager]? = try? persistenceManager.retrieve(from: "OwnedVillagers")
+        villagers = ownedVillagers ?? []
+        collectionView.reloadData()
     }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        isFiltering ? filteredVillagers.count : villagers.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: VillagerCell.reuseIdentifier, for: indexPath) as! VillagerCell
+        let activeVillagersArray = isFiltering ? filteredVillagers : villagers
+        cell.configure(with: activeVillagersArray[indexPath.row])
+        
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeVillagersArray = isFiltering ? filteredVillagers : villagers
+        
+        let villagerDetailsViewController = VillagerDetailsViewController(with: activeVillagersArray[indexPath.row])
+        present(villagerDetailsViewController, animated: true)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerReuseIdentifier, for: indexPath)
+        header.backgroundColor = .systemIndigo
+        
+        let headerTitle = UILabel().adjustedForAutoLayout()
+        headerTitle.text = "My Villagers"
+        headerTitle.textColor = .white
+        headerTitle.textAlignment = .left
+        headerTitle.font = UIFont.preferredFont(forTextStyle: .title3)
+        header.addSubview(headerTitle)
+
+        let horizontalPadding: CGFloat = 20
+        let verticalPadding: CGFloat = 16
+
+        NSLayoutConstraint.activate([
+            headerTitle.topAnchor.constraint(equalTo: header.topAnchor, constant: verticalPadding),
+            headerTitle.bottomAnchor.constraint(equalTo: header.bottomAnchor, constant: -verticalPadding),
+            headerTitle.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: horizontalPadding),
+            headerTitle.trailingAnchor.constraint(equalTo: header.trailingAnchor, constant: -horizontalPadding)
+        ])
+        
+        return header
+    }
+    
+    func createCompositionalLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { _, _ in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
+            layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.93), heightDimension: .estimated(200))
+            let layoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [layoutItem])
+            
+            let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
+            layoutSection.orthogonalScrollingBehavior = .groupPagingCentered
+            
+            return layoutSection
+        }
+        
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 20
+        config.boundarySupplementaryItems = [header]
+        layout.configuration = config
+        
+        return layout
+    }
+}
+
+extension VillagersCollectionViewController: VillagersTableViewControllerDelegate {
+    
+    func didTapCheckmarkButton() {
+        update()
     }
 }
