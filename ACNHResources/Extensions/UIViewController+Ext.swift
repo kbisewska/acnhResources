7dcfc440be36b4e97cd548725bd64f6cbe4f91cb
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-17 15:15:08 +0200

    Add title parameter to presentAlert method

--- ACNHResources/Extensions/UIViewController+Ext.swift
+++ ACNHResources/Extensions/UIViewController+Ext.swift
@@ -16,8 +16,8 @@ extension UIViewController {
         child.didMove(toParent: self)
     }
     
-    func presentAlert(with message: String) {
-        let alert = UIAlertController(title: "Something Went Wrong", message: message, preferredStyle: .alert)
+    func presentAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true)
     }
