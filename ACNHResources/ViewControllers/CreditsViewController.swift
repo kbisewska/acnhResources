eed714cec94b02e974192e188be851da2e175f04
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-25 13:50:34 +0200

    Create CreditsViewController

---
+++ ACNHResources/ViewControllers/CreditsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsViewController.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 25/06/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemIndigo
+    }
+}
