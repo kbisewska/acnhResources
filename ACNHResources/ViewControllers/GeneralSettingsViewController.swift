5ebd8b0cf37dbf4ac88888939e31d7e642d78518
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-25 13:49:04 +0200

    Create GeneralSettingsViewController

---
+++ ACNHResources/ViewControllers/GeneralSettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralSettingsViewController.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 25/06/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class GeneralSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemIndigo
+    }
+}
