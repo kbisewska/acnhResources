a52a9790d9064070fdbb908b68efc60985dba39b
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-26 11:23:35 +0200

    Create GeneralSettingsView

---
+++ ACNHResources/Views/GeneralSettingsView.swift
@@ -0,0 +1,13 @@
+//
+//  GeneralSettingsView.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 26/06/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class GeneralSettingsView: UIView {
+    
+}
