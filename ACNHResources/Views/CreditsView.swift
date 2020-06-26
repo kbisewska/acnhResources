c3e729796b1136b36f22b8fcb82fd0e48b9cff61
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-26 11:24:30 +0200

    Create CreditsView

---
+++ ACNHResources/Views/CreditsView.swift
@@ -0,0 +1,13 @@
+//
+//  CreditsView.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 26/06/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class CreditsView: UIView {
+    
+}
