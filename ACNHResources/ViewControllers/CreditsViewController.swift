bc8374197e214401e84562200689d6244387b5a0
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-26 14:44:09 +0200

    Configure CreditsViewController

--- ACNHResources/ViewControllers/CreditsViewController.swift
+++ ACNHResources/ViewControllers/CreditsViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class CreditsViewController: UIViewController {
+    
+    private let customView = CreditsView()
 
+    override func loadView() {
+        view = customView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemIndigo
+        view.backgroundColor = .systemBackground
+        
+        customView.titleLabel.text = "Credits"
+        customView.firstCreditsLabel.text = "All data images used in this app are the sole property of Nintendo."
+        customView.secondCreditsLabel.text = "Data used in the app come from ACNH API (http://acnhapi.com)."
+        customView.thirdCreditsLabel.text = "Tab bar icons made by Freepik and Vitaly Gorbachev from www.flaticon.com."
     }
 }
