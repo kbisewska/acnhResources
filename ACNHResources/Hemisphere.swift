73bfe1875d932e728257e4431777e9c55f5ea7f1
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-30 11:38:01 +0200

    Make Hemisphere enum Codable

--- ACNHResources/Hemisphere.swift
+++ ACNHResources/Hemisphere.swift
@@ -20,3 +20,34 @@ enum Hemisphere: CaseIterable {
         }
     }
 }
+
+extension Hemisphere: Codable {
+    
+    enum Key: CodingKey {
+        case rawValue
+    }
+    
+    enum CodingError: Error {
+        case unknownValue
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Key.self)
+        let rawValue = try container.decode(Int.self, forKey: .rawValue)
+        
+        switch rawValue {
+        case 0: self = .north
+        case 1: self = .south
+        default: throw CodingError.unknownValue
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Key.self)
+        
+        switch self {
+        case .north: try container.encode(0, forKey: .rawValue)
+        case .south: try container.encode(1, forKey: .rawValue)
+        }
+    }
+}
