392736723535fb9f9de6cadff3119db187331737
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-06-30 11:44:06 +0200

    Store selected picker view row in UserDefaults

--- ACNHResources/ViewControllers/GeneralSettingsViewController.swift
+++ ACNHResources/ViewControllers/GeneralSettingsViewController.swift
@@ -11,20 +11,35 @@ import UIKit
 class GeneralSettingsViewController: UIViewController {
     
     private let customView = GeneralSettingsView()
+    private let persistenceManager = PersistenceManager()
     
     override func loadView() {
         view = customView
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        configureLayout()
+        configurePicker()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureLayout() {
         view.backgroundColor = .systemBackground
         
         customView.titleLabel.text = "Settings"
         customView.settingsLabel.text = "Your Hemisphere:"
+    }
+    
+    func configurePicker() {
         customView.picker.delegate = self
         customView.picker.dataSource = self
+        customView.picker.selectRow(0, inComponent: 0, animated: true)
+        try? persistenceManager.store(value: Hemisphere.allCases[0], with: "Hemisphere")
     }
 }
 
@@ -47,5 +62,9 @@ extension GeneralSettingsViewController: UIPickerViewDataSource, UIPickerViewDel
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.textAlignment = .center
         return label
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        try? persistenceManager.store(value: Hemisphere.allCases[row], with: "Hemisphere")
     }
 }
