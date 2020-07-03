dd4882fc00a9cc6d93d6c5de41749dfda24269b4
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-07-03 12:47:52 +0200

    Add extensions tests

--- ACNHResourcesTests/ACNHResourcesTests.swift
+++ ACNHResourcesTests/ACNHResourcesTests.swift
@@ -19,16 +19,41 @@ class ACNHResourcesTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDateConverter() {
+        let date = Date.init(timeIntervalSince1970: 0)
+        
+        XCTAssertEqual(date.convertToDayMonthFormat(), "1/1")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testCapitalizingFirstLetter() {
+        let word = "test"
+        
+        XCTAssertEqual(word.capitalizeFirstLetter(), "Test")
     }
-
+    
+    func testSHA256Hash() {
+        let word = "test"
+        
+        XCTAssertEqual(word.sha256(), "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08")
+    }
+    
+    func testAddingSubviews() {
+        let view = UIView()
+        let subview1 = UIView()
+        let subview2 = UIView()
+        let subview3 = UIView()
+        
+        view.addSubviews(subview1, subview2, subview3)
+        
+        XCTAssertEqual(view.subviews, [subview1, subview2, subview3])
+    }
+    
+    func testAddingChildToUIViewController() {
+        let viewController = UIViewController()
+        let childViewController = UIViewController()
+        
+        viewController.add(childViewController)
+        
+        XCTAssertEqual(viewController.children, [childViewController])
+    }
 }
