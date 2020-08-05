2b0de4fd194722636eea56b3aa59f4880b831121
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-08-05 14:47:06 +0200

    Create BugsTableViewControllerSnapshotTests

---
+++ ACNHResourcesTests/BugsTableViewControllerSnapshotTests.swift
@@ -0,0 +1,32 @@
+//
+//  BugsTableViewControllerSnapshotTests.swift
+//  ACNHResourcesTests
+//
+//  Created by Kornelia Bisewska on 05/08/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+
+@testable import ACNHResources
+
+class BugsTableViewControllerSnapshotTests: XCTestCase {
+    
+    var sut: BugsTableViewController!
+    
+    override func setUp() {
+        super.setUp()
+        Current = .mock
+        sut = BugsTableViewController()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testBugsTableViewController() {
+        assertSnapshot(matching: sut, as: .image(on: .iPhoneXr))
+    }
+}
