7929155d92a164117afa17724fbbad63c74e5245
Kornelia Bisewska <bisewskakornelia@gmail.com>
2021-02-11 11:33:36 +0100

    Create NavigationBarCustomizable protocol

---
+++ ACNHResources/Protocols/NavigationBarCustomizable.swift
@@ -0,0 +1,26 @@
+//
+//  NavigationBarCustomizable.swift
+//  ACNHResources
+//
+//  Created by Kornelia Bisewska on 10/02/2021.
+//  Copyright © 2021 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+protocol NavigationBarCustomizable: AnyObject {}
+
+extension NavigationBarCustomizable where Self: UIViewController {
+    
+    func addNavigationItems(leftBarButtonTitle: String, leftBarButtonAction: Selector, rightBarButtonTitle: String, rightBarButtonAction: Selector) {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: leftBarButtonTitle, style: .plain, target: self, action: leftBarButtonAction)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: rightBarButtonTitle, style: .plain, target: self, action: rightBarButtonAction)
+    }
+    
+    func configureNavigationBar(forEnabledState state: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = state
+        navigationItem.leftBarButtonItem?.isEnabled = state
+        navigationItem.rightBarButtonItem?.isEnabled = state
+        navigationItem.searchController?.searchBar.isUserInteractionEnabled = state
+    }
+}
